160125968af50721a84e4b85f582e8d0682e7013
Virgil Cantu <cantu.sampsom@gmail.com>
2021-05-30 19:43:33 -0400

    added has many, and has many through relationship to UserCocktail and User, respectively, as well as a has many, and has many through relationship to CocktailIngredient and Ingredient, respectively, in Cocktail model

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -1,2 +1,6 @@
 class Cocktail < ApplicationRecord
+    has_many :user_cocktails
+    has_many :cocktails, through: :user_cocktails
+    has_many :cocktail_ingredients
+    has_many :cocktails, through: :cocktail_ingredients
 end
