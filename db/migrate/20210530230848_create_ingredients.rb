0683894df76421ba750dfbeb57a68b6b7b121906
Virgil Cantu <cantu.sampsom@gmail.com>
2021-05-30 19:10:21 -0400

    used rails g scaffold to generate Ingredient migration, model, routes, controller, and controller actions

---
+++ db/migrate/20210530230848_create_ingredients.rb
@@ -0,0 +1,12 @@
+class CreateIngredients < ActiveRecord::Migration[6.1]
+  def change
+    create_table :ingredients do |t|
+      t.string :name
+      t.string :category
+      t.string :type
+      t.string :origin
+
+      t.timestamps
+    end
+  end
+end
