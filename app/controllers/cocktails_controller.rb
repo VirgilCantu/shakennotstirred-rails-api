02d498da5f6dc7b52835ad098dbaca061a82e321
Virgil Cantu <cantu.sampsom@gmail.com>
2021-05-30 19:07:15 -0400

    used rails g scaffold to generate Cocktail migration, model, routes, controller, and controller actions

---
+++ app/controllers/cocktails_controller.rb
@@ -0,0 +1,51 @@
+class CocktailsController < ApplicationController
+  before_action :set_cocktail, only: [:show, :update, :destroy]
+
+  # GET /cocktails
+  def index
+    @cocktails = Cocktail.all
+
+    render json: @cocktails
+  end
+
+  # GET /cocktails/1
+  def show
+    render json: @cocktail
+  end
+
+  # POST /cocktails
+  def create
+    @cocktail = Cocktail.new(cocktail_params)
+
+    if @cocktail.save
+      render json: @cocktail, status: :created, location: @cocktail
+    else
+      render json: @cocktail.errors, status: :unprocessable_entity
+    end
+  end
+
+  # PATCH/PUT /cocktails/1
+  def update
+    if @cocktail.update(cocktail_params)
+      render json: @cocktail
+    else
+      render json: @cocktail.errors, status: :unprocessable_entity
+    end
+  end
+
+  # DELETE /cocktails/1
+  def destroy
+    @cocktail.destroy
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_cocktail
+      @cocktail = Cocktail.find(params[:id])
+    end
+
+    # Only allow a list of trusted parameters through.
+    def cocktail_params
+      params.require(:cocktail).permit(:name, :glassware, :ice, :image, :origin)
+    end
+end
---
+++ db/migrate/20210530230507_create_cocktails.rb
@@ -0,0 +1,13 @@
+class CreateCocktails < ActiveRecord::Migration[6.1]
+  def change
+    create_table :cocktails do |t|
+      t.string :name
+      t.string :glassware
+      t.string :ice
+      t.string :image
+      t.string :origin
+
+      t.timestamps
+    end
+  end
+end
