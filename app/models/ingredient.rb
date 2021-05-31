7e8680293811d582c4ea3a8f78e2bce3dc4abf15
Virgil Cantu <cantu.sampsom@gmail.com>
2021-05-30 20:49:41 -0400

    also added uniqueness validation for name attribute in Ingredient model

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -2,5 +2,6 @@ class Ingredient < ApplicationRecord
     has_many :cocktail_ingredients
     has_many :cocktails, through: :cocktail_ingredients
 
-    validates :name, presence: true
+    validates :name, presence: true, uniqueness: true
+    
 end
