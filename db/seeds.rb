3167b104b2457ac3690a568924b0d8d39d3202b6
Virgil Cantu <cantu.sampsom@gmail.com>
2021-05-30 22:20:57 -0400

    created 5 Cocktail objects in seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -37,3 +37,50 @@ joshua_clemens = User.create(
     username: "JoshuaClemens",
     location: "Seattle, WA"
 )
+
+#----------------------Cocktails--------------------#
+
+margarita = Cocktail.create(
+    name: "Margarita",
+    glassware: "Margarita",
+    ice: "Cubed",
+    image: "https://upload.wikimedia.org/wikipedia/commons/3/37/MargaritaReal.jpg",
+    origin: "Tijuana, Mexico",
+    preparation: "Rub the rim of the glass with the lime slice to make the salt stick to it. Shake the other ingredients with ice, then pour into the glass."
+)
+
+negroni = Cocktail.create(
+    name: "Negroni",
+    glassware: "Old-Fashioned",
+    ice: "Large Cube",
+    image: "https://upload.wikimedia.org/wikipedia/commons/c/c6/Negroni_served_in_Vancouver_BC.jpg",
+    origin: "Florence, Italy",
+    preparation: "Stir into glass over ice, garnish and serve."
+)
+
+mai_tai = Cocktail.create(
+    name: "Mai Tai",
+    glassware: "Old-Fashioned",
+    ice: "Crushed",
+    image: "https://upload.wikimedia.org/wikipedia/commons/7/74/Mai_Tai_Trader_Vics_Emeryville.jpg",
+    origin: "Oakland, CA",
+    preparation: "Shake all ingredients with ice. Strain into glass. Garnish and serve with straw."
+)
+
+mint_julep = Cocktail.create(
+    name: "Mint Julep",
+    glassware: "Julep Tin",
+    ice: "Crushed",
+    image: "https://upload.wikimedia.org/wikipedia/commons/5/55/Mint_Julep_im_Silberbecher.jpg",
+    origin: "Williamsburg, VA",
+    preparation: "In a julep tin gently muddle the mint, sugar and water. Fill the glass with cracked ice, add Bourbon and stir well until the glass is well frosted. Garnish with a mint sprig."
+)
+
+mojito = Cocktail.create(
+    name: "Mojito",
+    glassware: "Collins",
+    ice: "Crushed",
+    image: "https://upload.wikimedia.org/wikipedia/commons/0/01/Mojito98775.jpeg",
+    origin: "Havana, Cuba",
+    preparation: "Muddle mint leaves with sugar and lime juice. Add a splash of soda water and fill the glass with cracked ice. Pour the rum and top with soda water. Garnish and serve with straw."
+)
