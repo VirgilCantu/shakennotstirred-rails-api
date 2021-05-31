feecdc7ba586dc777b9b17cff7e691692ecadc35
Virgil Cantu <cantu.sampsom@gmail.com>
2021-05-31 00:53:49 -0400

    renamed type column to subcategory to adress problem with seeding data pertaining to reserved word

--- db/migrate/20210530230848_create_ingredients.rb
+++ db/migrate/20210530230848_create_ingredients.rb
@@ -3,7 +3,7 @@ class CreateIngredients < ActiveRecord::Migration[6.1]
     create_table :ingredients do |t|
       t.string :name
       t.string :category
-      t.string :type
+      t.string :subcategory
 
       t.timestamps
     end
