44aa2b4178b2bffcd87b3eb9db2c2b43e2cb2263
Virgil Cantu <cantu.sampsom@gmail.com>
2021-05-31 01:17:40 -0400

    fixed mistake in Cocktail model relationships in cocktail.rb: has many ingredients through cocktail ingredients

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -2,7 +2,7 @@ class Cocktail < ApplicationRecord
     has_many :user_cocktails
     has_many :cocktails, through: :user_cocktails
     has_many :cocktail_ingredients
-    has_many :cocktails, through: :cocktail_ingredients
+    has_many :ingredients, through: :cocktail_ingredients
 
     validates :name, presence: true, uniqueness: true
     validates :glassware, presence: true
