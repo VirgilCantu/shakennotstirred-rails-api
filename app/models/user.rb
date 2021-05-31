5bf11b2741ed0fe8405ee10b699f36e984622c3e
Virgil Cantu <cantu.sampsom@gmail.com>
2021-05-30 20:40:39 -0400

    added presence and uniqueness validations for username attribute in User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,6 @@ class User < ApplicationRecord
     has_many :cocktails, through: :user_cocktails
 
     validates :name, presence: true
+    validates :username, presence: true, uniqueness: true
 
 end
