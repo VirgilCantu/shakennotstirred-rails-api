edcb45eebd2651006e0af76c27510ff7706c59c4
Virgil Cantu <cantu.sampsom@gmail.com>
2021-05-30 21:01:05 -0400

    added presence and uniqueness validations for name attribute in Cocktail model

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -3,4 +3,6 @@ class Cocktail < ApplicationRecord
     has_many :cocktails, through: :user_cocktails
     has_many :cocktail_ingredients
     has_many :cocktails, through: :cocktail_ingredients
+
+    validates :name, presence: true, uniqueness: true
 end
