eefa28bd2b4ee865c3d95ad6a4c50891d71687fb
Virgil Cantu <cantu.sampsom@gmail.com>
2021-06-02 21:47:43 -0400

    ran migration to create quantity column in ingredients table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_31_014336) do
+ActiveRecord::Schema.define(version: 2021_06_03_013918) do
 
   create_table "cocktail_ingredients", force: :cascade do |t|
     t.integer "cocktail_id", null: false
@@ -38,6 +38,7 @@ ActiveRecord::Schema.define(version: 2021_05_31_014336) do
     t.string "subcategory"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "quantity"
   end
 
   create_table "user_cocktails", force: :cascade do |t|
