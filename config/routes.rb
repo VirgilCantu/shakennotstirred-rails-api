62908381a8aaa625846be6cf1e33d86964662e15
Virgil Cantu <cantu.sampsom@gmail.com>
2021-06-04 15:32:29 -0400

    deleted Users controller and all User routes, no longer needed

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   resources :ingredients
   resources :cocktails
-  resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
