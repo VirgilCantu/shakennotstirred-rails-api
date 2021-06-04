6cac829f75cc258fba710ff7c4bc322fc8095273
Virgil Cantu <cantu.sampsom@gmail.com>
2021-06-04 15:33:52 -0400

    deleted User and UserCocktails model, no longer needed

--- app/models/user.rb
+++
@@ -1,9 +0,0 @@
-class User < ApplicationRecord
-    has_many :user_cocktails
-    has_many :cocktails, through: :user_cocktails
-
-    validates :name, presence: true
-    validates :username, presence: true, uniqueness: true
-    validates :location, presence: true
-
-end
